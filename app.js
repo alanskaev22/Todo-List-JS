9ec411c66abfbe6cbaa8c45173a8035a85a97315
Alan Skaev <askaev@rccl.com>
2020-04-28 18:19:55 -0400

    Refactoring header & footer modules

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-let today = new Date();
-let newItems = [];
-let newItem = "";
-let currentDate = "";
+const newItems = [];
+const workItems = [];
+const Endpoints = {
+  HOMEPAGE: "/",
+  WORK: "/work",
+  ABOUT: "/about",
+};
 
-app.get("/", function (req, res) {
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
+let newItem;
+let currentDate;
 
-  currentDate = today.toLocaleDateString("en-US", options);
-
-  res.render("list", { currentDate: currentDate, newItems: newItems });
+app.get(Endpoints.HOMEPAGE, function (req, res) {
+  currentDate = date.getDay();
+  res.render("list", {
+    listTitle: currentDate,
+    newItems: newItems,
+    endpoint: Endpoints.HOMEPAGE,
+  });
 });
 
-app.post("/", (postReq, postRes) => {
+app.post(Endpoints.HOMEPAGE, (postReq, postRes) => {
   newItem = postReq.body.newItem;
   newItems.push(newItem);
-  postRes.redirect("/");
+  postRes.redirect(Endpoints.HOMEPAGE);
 });
 
+app.get(Endpoints.WORK, (req, res) => {
+  res.render("list", {
+    listTitle: "Work Items",
+    newItems: workItems,
+    endpoint: Endpoints.WORK,
+  });
+});
+
+app.post(Endpoints.WORK, (req, res) => {
+  newItem = req.body.newItem;
+  workItems.push(newItem);
+  res.redirect(Endpoints.WORK);
+});
+
+app.get(Endpoints.ABOUT, (req, res) => {
+  res.render("about");
+});
+// Run on port
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
