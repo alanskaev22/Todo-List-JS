7ec245e145cb7b0610068e77724756f7dc3516ea
Alan Skaev <askaev@rccl.com>
2020-04-28 13:00:58 -0400

    render ejs

--- app.js
+++ app.js
@@ -2,21 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let date = new Date();
+  let today = new Date();
 
-    let day = date.getDate();
-    res.send(`<h1>${day.toString()}</h1>`);
+  let currentDate = today.toLocaleDateString("en-US", options);
 
+  res.render("list", { currentDate: currentDate });
 });
 
+app.post("/", (postReq, postRes) => {
+  let newItem = postReq.body.newItem;
+  console.log(newItem);
+});
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
+  console.log("Server running on port 3000");
 });
+
+// Functions
